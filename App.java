e402f10f6cb13e54959afb4c27a787242a776b73
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2023-04-15 21:42:58 +0800

    Allow move in Y axis

--- App.java
+++ App.java
@@ -19,48 +19,57 @@ public class App {
         JPanel panel = new JPanel();
         panel.setFocusable(true);
         int baseWidth = 15;
-        //odd size
-        int[] ints = {4,1,3,2,6,9,10};
-        //even size
+        // odd size
+        int[] ints = { 4, 1, 3, 2, 6, 9, 10 };
+        // even size
         // int[] ints = {4,1,3,2,6,9,10,5};
         Block[] blocks = new Block[ints.length];
-        //Todo: User input
-        //Create blocks
+        // Todo: User input
+        // Create blocks
         for (int i = 0; i < ints.length; i++) {
-            blocks[i] = new Block(new Point((i+1) * baseWidth, 20), ints[i]);
+            blocks[i] = new Block(new Point((i + 1) * baseWidth, 20), ints[i]);
             panel.add(blocks[i]);
         }
-        
+
         panel.addKeyListener(new KeyListener() {
             final int todos = blocks.length;
-            int split = (int) Math.round(todos/2.0); //Must do float division
+            int split = (int) Math.round(todos / 2.0); // Must do float division
+
             public void move(Block b, boolean right) {
+                move(b, right, true);
+            }
+
+            public void move(Block b, boolean right, boolean down) {
                 Point p = b.getLocation();
-                int changeX = (int) ((p.getX() + (right ? 1:-1) * baseWidth * split));
-                b.setLocation(changeX,(int) p.getY() + b.getHeight());
+                int changeX = (int) ((p.getX() + (right ? 1 : -1) * baseWidth * split));
+                int changeY = (int) p.getY() + (down ? 1 : -1) * + b.getHeight();
+                b.setLocation(changeX, changeY);
                 panel.repaint();
             }
+
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() != KeyEvent.VK_RIGHT)
                     return;
-                
-                if (split > 0) {  //Split step
+
+                if (split > 0) { // Split step
                     int j = 0;
-                    //System.out.println(split);
+                    // System.out.println(split);
                     boolean right = false;
-                    while(j < todos) {
+                    while (j < todos) {
                         for (int i = 0; i < split && j < todos; i++) {
                             move(blocks[j++], right);
                         }
                         right = !right;
-                        /* for (int i = 0; i < split && j < todos; i++) {
-                            move(blocks[j++], true);
-                        } */
+                        /*
+                         * for (int i = 0; i < split && j < todos; i++) {
+                         * move(blocks[j++], true);
+                         * }
+                         */
                     }
-                    split/=2;
+                    split /= 2;
                 } else { // Merge step
-                    //todo
+                    // todo
                 }
 
                 panel.repaint();
