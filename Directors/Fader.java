367fda5215a8523ec66d970a79e87e08b37d553e
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2023-04-30 10:40:02 +0800

    Feat: undo step for Fader

--- Directors/Fader.java
+++ Directors/Fader.java
@@ -2,6 +2,7 @@ package Directors;
 
 import java.awt.event.KeyEvent;
 import java.util.Arrays;
+import java.util.Iterator;
 
 import Block.Block;
 import Block.Row;
@@ -9,19 +10,19 @@ import Block.Row;
 public class Fader extends RowDirector {
     public Fader(int[] ints, float fontSize) {
         super(ints, fontSize);
-        anim.interval = 200;
+        anim.interval = 100;
         mergeSort();
     }
 
     public Fader(int[] ints) {
         super(ints);
-        anim.interval = 200;
+        anim.interval = 100;
         mergeSort();
     }
 
     public Fader(Block[] blocks) {
         super(blocks);
-        anim.interval = 200;
+        anim.interval = 100;
         mergeSort();
     }
 
@@ -33,40 +34,70 @@ public class Fader extends RowDirector {
     }
 
     int rowI = 1;
+
     @Override
     public void addListeners() {
         // Activate by pressing right arrow key
         anim.onPress(KeyEvent.VK_RIGHT, this,
-            t -> {
-                if (rowI == rows.size()) {
-                    System.out.println("Animation end");
-                    return;
-                }
-
-                Row prev = rows.get(rowI-1);
-                Row cur = rows.get(rowI);
-                var iter = Arrays.stream(cur.blocks).iterator();
-                anim.every(tt -> {
-                    if (iter.hasNext()) {
-                        Block b = iter.next();
-                        b.show();
-                        for (Block p:prev.blocks) {
-                            if (p.isShown && b.value == p.value) {
-                                p.hide();
-                                break;
-                            }
-                        }
-                    } else {
-                        tt.cancel();
+                t -> {
+                    System.out.println("rowI="+rowI);
+                    if (rowI >= rows.size()) {
+                        System.out.println("Animation end");
+                        return;
                     }
-                }, 0);
-            rowI++;
-        });
 
-        //TODO: PREV
+                    Row prev = rows.get(rowI - 1);
+                    Row cur = rows.get(rowI);
+                    var iter = Arrays.stream(cur.blocks).iterator();
+                    anim.every(tt -> {
+                        if (iter.hasNext()) {
+                            Block b = iter.next();
+                            b.show();
+                            for (Block p : prev.blocks) {
+                                if (p.isShown && b.value == p.value) {
+                                    p.hide();
+                                    break;
+                                }
+                            }
+                        } else {
+                            tt.cancel();
+                        }
+                    }, 0);
+                    rowI++;
+                });
+
         // Activate by pressing left arrow key
-        /* anim.onPress(KeyEvent.VK_LEFT, this,
-                t -> undoLastRow()); */
+        anim.onPress(KeyEvent.VK_LEFT, this,
+        t -> onPrev());
+        
+    }
+
+    public void onPrev() {
+        System.out.println("rowI="+rowI);
+        if (rowI <= 1) {
+            rowI = 1;
+            System.out.println("Animation back to start");
+            return;
+        }
+
+        Row prev = rows.get(rowI-2);
+        Row cur = rows.get(rowI-1);
+        rowI--;
+        Iterator<Block> iter = Arrays.stream(cur.blocks).iterator();
+        anim.every(tt -> {
+            if (iter.hasNext()) {
+                Block b = iter.next();
+                b.hide();
+                for (Block p : prev.blocks) {
+                    if (!p.isShown && b.value == p.value) {
+                        p.show();
+                        break;
+                    }
+                }
+            } else {
+                tt.cancel();
+            }
+        }, 0);
     }
 
     @Override
